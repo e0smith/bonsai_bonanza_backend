96a346ee61ddcacb90dd3f258e318cbce8d32a8f
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-24 19:48:10 -0400

    user and tree scaffold

---
+++ db/migrate/20210524234526_create_trees.rb
@@ -0,0 +1,16 @@
+class CreateTrees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trees do |t|
+      t.string :name
+      t.string :pruning
+      t.string :wiring
+      t.string :repotting
+      t.string :propagation
+      t.string :disease
+      t.string :placement
+      t.string :fertilizing
+
+      t.timestamps
+    end
+  end
+end
