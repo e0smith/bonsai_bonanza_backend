c7c3aa2efb66f804728b9129b18ddf2754c770ee
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-24 20:13:57 -0400

    partial API data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,141 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+juniper = Tree.create(
+    name: "Juniper",
+    placement: " Place the tree outside, year-round, in a bright location with lots of sunlight. The Juniper cannot live indoors. During the winter protect the tree once temperatures drop below 15 °F (-10 °C). Some species change their foliage color during frosty periods to a purplish brown which is a part of their internal frost protection mechanism. Don’t worry they will turn green again in spring.",
+    fertilizing: "Use normal organic fertilizer pellets every month during the growing season or a liquid fertilizer every week. If you’d like to see strong growth you can apply some higher nitrogen levels in the spring.",
+    wiring: ""
+    watering: "Be careful not to overwater, as the juniper’s roots don't like soil wetness. Before you water, the soil should slightly dry. Misting the tree can be done regularly, especially after the tree has been repotted because it benefits from air humidity.",
+    pruning: "To develop the foliage pads, long shoots that stick out of the silhouette can be pinched or cut at the base with sharp scissors throughout the growing season. Do not trim the juniper like a hedge because the removal of all growing tips will weaken the tree and the cut will turn the needles brown. When the foliage pads become too dense they must be thinned out with sharp scissors at the base. The Juniper Bonsai is generally a strong tree that also withstands aggressive pruning very well. But it cannot bud again from bare tree parts, so take care that there is some foliage left on every branch you wish to keep alive.",
+    repotting: " Repot the Juniper Bonsai tree once every two years using a basic, or slightly more draining soil mixture. Very old trees can be repotted at longer intervals. Do not prune the roots too aggressively.",
+    propagation: "If junipers are well cared for and placed in an ideal location they are very resistant to pests. Make sure not to allow foliage pads to get too dense otherwise, pests can settle in them more easily. During winter the junipers must be kept in a place with enough light and they must be checked for pests regularly even in winter. Junipers can sometimes get infested with spider mites, juniper scale, juniper aphids, and juniper needle miners as well as juniper webworms. Traditional insecticide/miticide sprays will help but if you want to get rid of pests, you should investigate why the tree was prone to infestation. Fungal rust diseases are a big problem. Juniper species have different levels of susceptibility to rust fungus. Some are even considered resistant to fungal rust diseases. As a rule of thumb, the blue-green junipers are more resistant than those with yellowish-green foliage. The Japanese junipers are also not infested often. You can find files that list many juniper species and cultivars and their susceptibility/resistance level to rust fungus on the internet. The rust fungus infests the junipers permanently and cannot be cured. It causes swellings that erupt with brown galls. During winter, particularly in rainy weather, the galls produce large, orange, gelatin-like tendrils, full of spores that infest the leaves of pear trees or hawthorn/crabapples. You can identify the fungus when you see orange spots on the pear leaves. In late summer brownish proliferations grow from the bottom-sides of the leaves which release spores that infest junipers. While the pear trees in most cases are not fatally affected – they are newly infected each year, and they can even be treated successfully with a fungicide. An infected juniper normally cannot be cured. The visibly infested branches die in most cases and the fungus can emerge on other tree parts. Removing the parts with the swellings and galls is no guarantee that the fungus will not reappear. Some people have a different opinion, but it’s best to burn rust-infested juniper immediately or put it into the garbage instead of your compost heap.",
+    disease: " If junipers are well cared for and placed in an ideal location they are very resistant to pests. Make sure not to allow foliage pads to get too dense otherwise, pests can settle in them more easily. During winter the junipers must be kept in a place with enough light and they must be checked for pests regularly even in winter. Junipers can sometimes get infested with spider mites, juniper scale, juniper aphids, and juniper needle miners as well as juniper webworms. Traditional insecticide/miticide sprays will help but if you want to get rid of pests, you should investigate why the tree was prone to infestation. Fungal rust diseases are a big problem. Juniper species have different levels of susceptibility to rust fungus. Some are even considered resistant to fungal rust diseases. As a rule of thumb, the blue-green junipers are more resistant than those with yellowish-green foliage. The Japanese junipers are also not infested often. You can find files that list many juniper species and cultivars and their susceptibility/resistance level to rust fungus on the internet. The rust fungus infests the junipers permanently and cannot be cured. It causes swellings that erupt with brown galls. During winter, particularly in rainy weather, the galls produce large, orange, gelatin-like tendrils, full of spores that infest the leaves of pear trees or hawthorn/crabapples. You can identify the fungus when you see orange spots on the pear leaves. In late summer brownish proliferations grow from the bottom-sides of the leaves which release spores that infest junipers. While the pear trees in most cases are not fatally affected – they are newly infected each year, and they can even be treated successfully with a fungicide. An infected juniper normally cannot be cured. The visibly infested branches die in most cases and the fungus can emerge on other tree parts. Removing the parts with the swellings and galls is no guarantee that the fungus will not reappear. Some people have a different opinion, but it’s best to burn rust-infested juniper immediately or put it into the garbage instead of your compost heap." 
+    )
+
+trident_maple = Tree.create(
+    name: "Trident Maple",
+    fertilizing: "During the growing season fertilize the tree once a month with solid organic fertilizer or every week with a liquid product.",
+    watering: "Trident maples grow vigorously and consume a lot of water during the growing season. Always water in time, but do not overwater. Don't use very calcareous water, rain water is preferable if it can be obtained.",
+    pruning: "The new shoots in spring are usually allowed to grow until they have developed several leaf pairs and then they are shortened leaving just one pair. The apex tends to grow stronger than the lower branches, so balance the tree's growth by pruning the top more and letting weaker branches catch up. New shoots appear during the entire growing season and must be shortened continuously. On mature bonsai, pinching the young shoots very early can be necessary, to prevent the finer twigs of the outer canopy from thickening. Pruning of large branches or trunks should be done in summer when the cut wounds begin to heal immediately. Vigorous trident maples can close even larger wounds within a few years. Always seal wounds with cut paste to avoid fungi entering and to prevent die-back of the bark.
+    Healthy compact trees can be defoliated partly or totally in summer, in regions with long warm growth periods even several times a year, to promote delicate ramification and smaller leaves and prevent inner twigs from dying from lack of light. On trees which are not defoliated still the largest leaves can be removed at any time. Autumn, when the leaves fall, is a good time to shorten the twigs and remove those which have grown in odd directions and all but two growing from the same point. The winter buds of trident maple bonsai are tiny and sometimes you might need a magnifying glass to see in which direction they point to decide where to cut, in order to make the new shoots grow into the desired direction in the following year. Younger branches and twigs can be wired and shaped quite well during winter dormancy, but check and remove the wire in time. Because of the strong growth wire can scar the bark very soon after the spring shoots have emerged. Guy wires are a good option to position stiff older branches.",
+    repotting: "The trident maple should be repotted every two or three years in early spring, large old specimen less often. The roots grow strongly and can be pruned heavily. Use a well-draining soil mix with a pH value between 5 and 7."
+    propagation: "Trident maples can be grown from seeds, cuttings or air-layers which root easily. For more detailed information on these techniques, try our Bonsai tree care section.",
+    disease: "Vigorous trident maples are hardly attacked by pests and diseases. Aphids, scale, caterpillars or spider mites can sometimes occur and powdery mildew or leaf spot fungi. Use a specific pesticide in these cases. Take care to seal cut wounds immediately to prevent Verticillium and other harmful fungi from entering. Root rot can occur when the tree is overwatered. Frequent watering with calcareous water can cause chlorosis."
+    )
+
+ficus = Tree.create(
+    name: "Ficus",
+    fertilizing: "Fertilize every two weeks during summer, and every four weeks during winter if the growth doesn't stop. Liquid fertilizer can be used as well as organic fertilizer pellets.",
+    wiring: "Wiring and bending thin to medium Ficus branches is easy due to their flexibility, but you should check the wires regularly as they can cut into the bark very quickly. Strong branches should be shaped with guy-wires because they can be left on the tree for a much longer period.",
+    watering: "The Ficus should be watered normally, which means it should be given water generously whenever the soil gets slightly dry. The Bonsai Ficus prefers room temperature soft water and it can tolerate occasional over, or underwatering. We advise daily misting to maintain humidity, but too much misting can create fungal problems. The warmer the placement of the fig during winter the more water it needs. If it’s kept in a cooler place it only needs to be kept slightly moist.",
+    pruning: "Regular pruning is necessary to retain the tree’s shape. Prune back to 2 leaves after 6-8 leaves have grown. Leaf pruning (defoliation) can be used to reduce leaf size, as some Ficus Bonsai species normally grow large leaves. If a considerable thickening of the trunk is desired, the Ficus can be left to grow freely for one or two years. The strong cuts that are necessary afterward don't affect the Ficus' health and new shoots will grow from old wood. Larger wounds should be covered with cut paste. ",
+    repotting: "Repot your Ficus tree during the spring, every other year, using a basic Bonsai soil mixture. Ficus tolerates root-pruning very well.",
+    propagation: "Cuttings can be planted at any time of the year, but they have the highest success rate during mid-summer growth. Air-layering will work best during spring, in April through May. In most cases, springtime is the best time for planting Ficus seeds.",
+    disease: "Fig species are quite resistant against pests, but they are still susceptible to several issues depending on their location, and time of year, especially in the winter. Dry air and a lack of light weakens the Bonsai Ficus and often result in leaf drop. In poor conditions like these, they are sometimes infested with scale or spider mites. Placing customary insecticide sticks into the soil or spraying insecticide/miticide will get rid of the pests, but a weakened Ficus tree’s living conditions must be improved. Using plant lamps 12 to 14 hours a day, and frequently misting the leaves will help in the recovery process." 
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
+
+    = Tree.create(
+    name: "",
+    fertilizing: "",
+    wiring: "", watering: "",
+    pruning: "", repotting: "",
+    propagation: "",
+    disease: ""
+    )
